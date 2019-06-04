a146e9ca4dede0a9b52fd08ee3b4ee841b0d139d
Martin Panin <yourself@coralself.com>
2019-06-05 00:21:48 +0300

    initial props

--- pages/category.js
+++ pages/category.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import  Link from "next/link";
 import Head from "../components/Head";
-import axios from "axios";
 import Loading from "../components/Loading";
-
+import 'isomorphic-unfetch';
 
 
 class Category extends Component {
@@ -12,29 +11,27 @@ class Category extends Component {
         this.state = {
             title:"Category",
             categories:[],
-            loading:true
+            loading:false
         }
+        
     }
-    componentDidMount(){
-    axios.get('http://167.99.16.124:5001/categories')
-        .then(function (response) {
-        const categories = response.data;
-        this.setState({categories:categories, loading:false})
-    }.bind(this))
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
+
+    static async getInitialProps () {
+        // eslint-disable-next-line no-undef
+        const res = await fetch('http://167.99.16.124:5001/categories/')
+        const json = await res.json()
+        return { categories: json }
     }
-    render(cat=this.state.categories) {
+    
+
+    render(cat=this.props.categories) {
         if(!this.state.loading){
         return(
             <React.Fragment>
                 <Head title={this.state.title} />
-                <h2 className="categoryList-title">Available categories ({cat.length})</h2>
                 {cat.map((cat) => {
                     return (
-                        <Link href={'/category/itemlist/?title='+ cat.id} key={cat.id} >
+                        <Link prefetch href={{pathname: `/category/${cat.name}`, query: { id: cat.id }}} key={cat.id} >
                             <a className={"categoryItem"} style={{backgroundImage: `url("http://167.99.16.124:5001${cat.images.url}")`}}><span>{cat.name}</span></a>
                         </Link>
                     );
